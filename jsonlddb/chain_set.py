660f2766fc2ab9750b6091e4067e3d4810f7939c
Daniel Clarke <u8sand@gmail.com>
2020-01-02 13:26:39 -0500

    Fixed chainset with 100% test coverage

--- jsonlddb/chain_set.py
+++ jsonlddb/chain_set.py
@@ -10,35 +10,52 @@ def is_set(v):
 
 def chain_set_union(generators):
   # Process sets first
-  S_ = set()
+  S = set()
   iterators = collections.deque()
   for gen in generators:
-    if isinstance(gen, set):
+    if is_set(gen):
       # Yield 'em as we get 'em
-      for s in gen - S_:
+      for s in gen - S:
         yield s
-      S_ |= gen
+      S |= gen
     else:
       iterators.append(iter(gen))
   # No actual iterators? we're already done
   if not iterators:
     return
   # Otherwise return a generator which yields the current elements
-  def G():
-    S = S_
-    # Proceed with yielding additional union elements
-    while iterators:
-      it = iterators.popleft()
-      try:
-        v = next(it)
-        if v not in S:
-          yield v
-          S.add(v)
-        iterators.append(it)
-      except StopIteration:
-        pass
-  #
-  return G()
+  while iterators:
+    it = iterators.popleft()
+    try:
+      v = next(it)
+      if v not in S:
+        yield v
+        S.add(v)
+      iterators.append(it)
+    except StopIteration:
+      pass
+
+def chain_set_intersection_from_iterable(iterators, S={}, impossible=set()):
+  n_iterators = len(iterators)
+  S = {}
+  while iterators:
+    i, it = iterators.popleft()
+    try:
+      v = next(it)
+      # only if we know it's not impossible
+      if v not in impossible:
+        # create a new element if it doesn't yet exist
+        if S.get(v) is None:
+          S[v] = set()
+        # has this iterator not registered this element yet?
+        if i not in S[v]:
+          S[v].add(i)
+          # all generators registered--we can yield it
+          if len(S[v]) == n_iterators:
+            yield v
+      iterators.append((i, it))
+    except StopIteration:
+      pass
 
 def chain_set_intersection(generators):
   # Process sets first
@@ -49,7 +66,7 @@ def chain_set_intersection(generators):
   iterators = collections.deque()
   # Shortest to largest will be the most efficient way to process these
   for gen in generators:
-    if isinstance(gen, set):
+    if is_set(gen):
       if S_ is None:
         S_ = gen
       else:
@@ -63,8 +80,14 @@ def chain_set_intersection(generators):
   if not iterators:
     return S_
   # Otherwise proceed with checking generators
-  def G():
+  if S_:
+    n_iterators = len(iterators) + 1
+    S = {s: {-1} for s in S_}
+  else:
+    n_iterators = len(iterators)
     S = {}
+  #
+  def G():
     while iterators:
       i, it = iterators.popleft()
       try:
@@ -78,7 +101,7 @@ def chain_set_intersection(generators):
           if i not in S[v]:
             S[v].add(i)
             # all generators registered--we can yield it
-            if len(S[v]) == len(generators):
+            if len(S[v]) == n_iterators:
               yield v
         iterators.append((i, it))
       except StopIteration:
---
+++ jsonlddb/test_chain_set.py
@@ -0,0 +1,91 @@
+from .chain_set import chain_set_union, chain_set_intersection
+
+def assert_chain_set_eq(gen0, gen1):
+  V0 = {}
+  for v in gen0:
+    if V0.get(v) is None:
+      V0[v] = 0
+    V0[v] += 1
+  #
+  V1 = {}
+  for v in gen1:
+    if V1.get(v) is None:
+      V1[v] = 0
+    V1[v] += 1
+  #
+  assert V0 == V1
+  assert all(v == 1 for v in V0.values())
+
+def test_chain_set_union():
+  # only generators
+  assert_chain_set_eq(
+    chain_set_union([
+      range(10),
+      range(5, 15),
+      range(10, 20),
+    ]),
+    range(20),
+  )
+  # 1 set mixture
+  assert_chain_set_eq(
+    chain_set_union([
+      set(range(15, 25)),
+      range(10),
+      range(5, 15),
+      range(10, 20),
+    ]),
+    range(25),
+  )
+  # 2 set mixture
+  assert_chain_set_eq(
+    chain_set_union([
+      set(range(15, 25)),
+      set(range(10)),
+      range(5, 15),
+      range(10, 20),
+    ]),
+    range(25),
+  )
+  # only sets
+  assert_chain_set_eq(
+    chain_set_union([
+      set(range(10)),
+      set(range(10, 20)),
+    ]),
+    range(20),
+  )
+
+def test_chain_set_intersection():
+  # only generators
+  assert_chain_set_eq(
+    chain_set_intersection([
+      range(10),
+      range(5, 15),
+    ]),
+    set(range(5, 10)),
+  )
+  # 1 set mixture
+  assert_chain_set_eq(
+    chain_set_intersection([
+      set(range(10)),
+      range(5, 15),
+    ]),
+    set(range(5, 10)),
+  )
+  # 2 set mixture
+  assert_chain_set_eq(
+    chain_set_intersection([
+      set(range(10)),
+      set(range(5, 15)),
+      range(0, 15),
+    ]),
+    set(range(5, 10)),
+  )
+  # only sets
+  assert_chain_set_eq(
+    chain_set_intersection([
+      set(range(10)),
+      set(range(5, 15)),
+    ]),
+    set(range(5, 10)),
+  )
