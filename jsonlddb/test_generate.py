14ebb1878aef0a103fbc18b8d4cdc499405aab5f
Daniel Clarke <u8sand@gmail.com>
2020-01-03 13:24:14 -0500

    Add generate test

---
+++ jsonlddb/test_generate.py
@@ -0,0 +1,9 @@
+from jsonlddb.generate import random_jsonld
+from jsonlddb.oop import JsonLDDatabase
+
+def test_generate():
+  jsonld = list(random_jsonld(10, 5, 1, 5, 1, 5))
+  assert len(jsonld) == 10
+  db = JsonLDDatabase()
+  db.update(jsonld)
+  assert len(list(db['@type'])) <= 5
