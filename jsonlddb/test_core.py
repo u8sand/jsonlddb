8d5e21d0cf19d0bd4db646bc1ce92a6be59c17b8
Daniel Clarke <u8sand@gmail.com>
2020-01-04 18:45:10 -0500

    Move framing tests to test_oop

--- jsonlddb/test_core.py
+++ jsonlddb/test_core.py
@@ -1,5 +1,5 @@
 import pytest
-from jsonlddb.core import jsonld_frame_with_multi_index, jsonld_index_insert_triples, jsonld_to_triples, jsonld_index_remove_triples, JsonLDIndex
+from jsonlddb.core import jsonld_to_triples
 from jsonlddb.rdf import RDFTerm, RDFTermType
 
 def test_jsonld_triple_conversion():
@@ -35,81 +35,3 @@ def test_jsonld_triple_conversion():
     list(jsonld_to_triples({
       'k': {1, 2},
     }))
-
-def test_jsonld_frame_with_multi_index():
-  jsonld = [
-    {
-      '@id': '0',
-      '@type': 'Person',
-      'owns': {'@id': '4', '@type': 'Car', 'model': 'S'},
-      'spouseOf': [{'@id': '1'}],
-    },
-    {
-      '@id': '1',
-      '@type': 'Person',
-      'spouseOf': [{'@id': '0'}],
-    },
-    {
-      '@id': '2',
-      '@type': 'Person',
-      'childOf': [{'@id': '0'}, {'@id': '1'}],
-    },
-    {
-      '@id': '3',
-      '@type': 'Person',
-      'owns': {'@id': '5', '@type': 'Car', 'model': '3'},
-      'childOf': [{'@id': '0'}, {'@id': '1'}],
-    },
-    {
-      '@id': '6',
-      '@type': 'Car',
-      'model': 'X',
-    },
-  ]
-  index = jsonld_index_insert_triples(JsonLDIndex(), jsonld_to_triples(jsonld))
-
-  # Empty query should return all subjects
-  query = {}
-  expected = index.spo.keys()
-  result = set(jsonld_frame_with_multi_index([index], query))
-  assert result == expected, result
-
-  # Show all cars that are owned by a person
-  query = {'@type': 'Car', '~owns': { '@type': 'Person' }}
-  expected = {RDFTerm(RDFTermType.IRI, '4'), RDFTerm(RDFTermType.IRI, '5')}
-  result = set(jsonld_frame_with_multi_index([index], query))
-  assert result == expected, result
-
-  # Show all cars that are owned by a person who is a child of another person who owns a car
-  query = {'@type': 'Car', '~owns': {'@type': 'Person', 'childOf': { '@type': 'Person', 'owns': { '@type': 'Car' } }}}
-  expected = {RDFTerm(RDFTermType.IRI, '5')}
-  result = set(jsonld_frame_with_multi_index([index], query))
-  assert result == expected, result
-
-  # Someone who is the spouse of someone who owns something
-  #  and is the parent of someone that owns something
-  query = { '~spouseOf': { 'owns': {} }, '~childOf': { 'owns': {} } }
-  expected = {RDFTerm(RDFTermType.IRI, '1')}
-  result = set(jsonld_frame_with_multi_index([index], query))
-  assert result == expected, result
-
-  # Move car ownership of person 3 to person 2
-  index = jsonld_index_remove_triples(index, jsonld_to_triples({
-    '@id': '3',
-    'owns': { '@id': '5' }
-  }))
-  index = jsonld_index_insert_triples(index, jsonld_to_triples({
-    '@id': '2',
-    'owns': { '@id': '5' }
-  }))
-  # Assert that it worked
-  query = { '@type': 'Person', 'owns': { '@id': '5' } }
-  expected = {RDFTerm(RDFTermType.IRI, '2')}
-  result = set(jsonld_frame_with_multi_index([index], query))
-  assert result == expected, result
-
-  # Query all car models that are owned
-  query = { '~model': { '@type': 'Car', '~owns': {} } }
-  expected = {RDFTerm(RDFTermType.LITERAL, 'S'), RDFTerm(RDFTermType.LITERAL, '3')}
-  result = set(jsonld_frame_with_multi_index([index], query))
-  assert result == expected, result
---
+++ jsonlddb/test_oop.py
@@ -0,0 +1,89 @@
+from jsonlddb.oop import JsonLDDatabase
+
+def test_jsonlddb_framing():
+  db = JsonLDDatabase().update([
+    {
+      '@id': '0',
+      '@type': 'Person',
+      'owns': {'@id': '4', '@type': 'Car', 'model': 'S'},
+      'spouseOf': [{'@id': '1'}],
+    },
+    {
+      '@id': '1',
+      '@type': 'Person',
+      'spouseOf': [{'@id': '0'}],
+    },
+    {
+      '@id': '2',
+      '@type': 'Person',
+      'childOf': [{'@id': '0'}, {'@id': '1'}],
+    },
+    {
+      '@id': '3',
+      '@type': 'Person',
+      'owns': {'@id': '5', '@type': 'Car', 'model': '3'},
+      'childOf': [{'@id': '0'}, {'@id': '1'}],
+    },
+    {
+      '@id': '6',
+      '@type': 'Car',
+      'model': 'X',
+    },
+  ])
+
+  # Empty query should return all subjects
+  query = {}
+  expected = {'0', '1', '2', '3', '4', '5', '6'}
+  result = set(db[query]['@id'])
+  assert result == expected, result
+
+  # Literal should expand to ~ {}
+  expected = {'Car', 'Person'}
+  result = set(db['@type']['@id'])
+  assert result == expected, result
+
+  # Show all cars that are owned by a person
+  query = {'@type': 'Car', '~owns': { '@type': 'Person' }}
+  expected = {'4', '5'}
+  result = set(db[query]['@id'])
+  assert result == expected, result
+
+  # Show all cars that are owned by a person who is a child of another person who owns a car
+  query = {'@type': 'Car', '~owns': {'@type': 'Person', 'childOf': { '@type': 'Person', 'owns': { '@type': 'Car' } }}}
+  expected = {'5'}
+  result = set(db[query]['@id'])
+  assert result == expected, result
+
+  # Someone who is the spouse of someone who owns something
+  #  and is the parent of someone that owns something
+  query = { '~spouseOf': { 'owns': {} }, '~childOf': { 'owns': {} } }
+  expected = {'1'}
+  result = set(db[query]['@id'])
+  assert result == expected, result
+
+  # Move car ownership of person 3 to person 2
+  index = db.remove({
+    '@id': '3',
+    'owns': { '@id': '5' }
+  })
+  index = db.update({
+    '@id': '2',
+    'owns': { '@id': '5' }
+  })
+  # Assert that it worked
+  query = { '@type': 'Person', 'owns': { '@id': '5' } }
+  expected = {'2'}
+  result = set(db[query]['@id'])
+  assert result == expected, result
+
+  # Query all car models that are owned
+  query = { '~model': { '@type': 'Car', '~owns': {} } }
+  expected = {'S', '3'}
+  result = set(db[query])
+  assert result == expected, result
+
+  # Query models S and X
+  query = { '@type': 'Car', 'model': ['S', 'X'] }
+  expected = {'4', '6'}
+  result = set(db[query]['@id'])
+  assert result == expected
