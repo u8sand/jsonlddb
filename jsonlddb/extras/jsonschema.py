2f512b5496595e562576bc4d189e30abd1d9d5cf
Daniel Clarke <u8sand@gmail.com>
2020-02-01 13:31:18 -0500

    Completed jsonschema export together with tests

--- jsonlddb/extras/jsonschema.py
+++ jsonlddb/extras/jsonschema.py
@@ -1,6 +1,7 @@
 import re
+from jsonlddb.core import utils, json
 
-def to_jsonschema(db):
+def to_jsonschema(db, additionalProperties=False):
   schema = {
     'components': {
       'Json': {
@@ -8,7 +9,7 @@ def to_jsonschema(db):
           { 'type': 'string' },
           { 'type': 'number' },
           { 'type': 'boolean' },
-          { 'type': 'array'
+          { 'type': 'array',
             'items': {
               '$ref': '#/components/Json'
             } },
@@ -39,8 +40,8 @@ def to_jsonschema(db):
                   'keyType': 'string',
                   'valueType': {
                     'oneOf': [
-                      '$ref': '#/components/JsonLD',
-                      '$ref': '#/components/Json',
+                      { '$ref': '#/components/JsonLD' },
+                      { '$ref': '#/components/Json' },
                     ]
                   }
                 }
@@ -71,34 +72,73 @@ def to_jsonschema(db):
           'enum': [ldType],
         },
       },
-      'additionalProperties': {
+      'patternProperties': {
         '^.*$': {
           'keyType': 'string',
           'valueType': '#/components/Json',
         },
       },
+      'additionalProperties': additionalProperties,
       'required': [],
     }
     for pred in preds:
-      pyTypes = {type(v) for v in db[{'@type': ldType}][pred]}
-      if not pyTypes:
+      pyTypes = {(utils.isLiteral(v), type(v)) for v in db[{'@type': ldType}][pred]}
+      predProps = {'anyOf': set()}
+      n_pyTypes = len(pyTypes - {(False, type(None)), (True, type(None))})
+      if n_pyTypes == 0:
         continue
-      predProps = {'oneOf': []}
-      for pytype in (pyTypes - {type(None)}):
-        if pytype == str:
-          predProps['oneOf'].append({'type': 'string'})
-        elif pytype == bool:
-          predProps['oneOf'].append({'type': 'boolean'})
-        elif pytype == int or pytype == float:
-          predProps['oneOf'].append({'type': 'number'})
+      elif n_pyTypes == 1:
+        isLiteral, pyType = next(iter(pyTypes))
+        if isLiteral:
+          if pyType == str:
+            predProps['anyOf'].add(json.JSON({'type': 'string'}))
+            predProps['anyOf'].add(json.JSON({'type': 'array', 'item': 'string'}))
+          elif pyType == bool:
+            predProps['anyOf'].add(json.JSON({'type': 'boolean'}))
+            predProps['anyOf'].add(json.JSON({'type': 'array', 'item': 'boolean'}))
+          elif pyType == int or pyType == float:
+            predProps['anyOf'].add(json.JSON({'type': 'number'}))
+            predProps['anyOf'].add(json.JSON({'type': 'array', 'item': 'number'}))
+          else:
+            predProps['anyOf'].add(json.JSON({'$ref': '#/components/Json'}))
         else:
-          predProps['oneOf'].append({'$ref': '#/components/JsonLD'})
-      if len(predProps['oneOf']) == 0:
+          predProps['anyOf'].add(json.JSON({
+            '$ref': '#/components/JsonLD',
+          }))
+          objs = db[{'@type': ldType}]
+          fLdTypes = set(objs[pred]['@type'])
+          for fLdType in fLdTypes:
+            if any(len(record[pred][{'@type': fLdType}]) > 1 for record in objs):
+              # m2m
+              predProps['anyOf'].add(json.JSON({
+                'type': 'array',
+                'items': {
+                  '$ref': '#/components/' + fLdType,
+                }
+              }))
+            else:
+              predProps['anyOf'].add(json.JSON({
+                '$ref': '#/components/JsonLD',
+              }))
+              predProps['anyOf'].add(json.JSON({
+                '$ref': '#/components/' + fLdType,
+              }))
+          #
+        #
+      else:
+        predProps['anyOf'].add(json.JSON({
+          '$ref': '#/components/JsonLD',
+        }))
+      #
+      if len(predProps['anyOf']) == 0:
         continue
-      elif len(predProps['oneOf']) == 1:
-        predProps = predProps['oneOf'][0]
+      elif len(predProps['anyOf']) == 1:
+        predProps = dict(predProps['anyOf'][0])
+      else:
+        predProps['anyOf'] = [dict(prop) for prop in predProps['anyOf']]
+      #
       ldTypeSchema['properties'][pred] = predProps
-      if type(None) not in pyTypes:
+      if {(False, type(None)), (True, type(None))} & pyTypes:
         ldTypeSchema['required'].append(pred)
     if not ldTypeSchema['required']:
       del ldTypeSchema['required']
--- jsonlddb/extras/test_jsonschema.py
+++ jsonlddb/extras/test_jsonschema.py
@@ -1,6 +1,15 @@
+from jsonschema import RefResolver, Draft6Validator
 from jsonlddb import JsonLDDatabase
 from jsonlddb.extras import generate, jsonschema
 
 def test_to_jsonschema():
-  db = JsonLDDatabase().update(list(generate.random_jsonld(100, 5, 2, 5, 2, 5)))
-  jsonschema.to_jsonschema(db)
+  jsonld = list(generate.random_jsonld(100, 5, 2, 5, 2, 5))
+  db = JsonLDDatabase().update(jsonld)
+  schemas = jsonschema.to_jsonschema(db)
+  resolver = RefResolver.from_schema(schemas)
+  validators = {}
+  for component, schema in schemas['components'].items():
+    validators[component] = Draft6Validator(schema, resolver=resolver)
+  #
+  for instance in jsonld:
+    validators[instance['@type']].validate(instance)
