ad9036ac47e1e31304b6d338940e7ef82eca190a
Daniel Clarke <u8sand@gmail.com>
2020-02-15 10:53:06 -0500

    Use random data to test datapackage

--- jsonlddb/extras/test_datapackage.py
+++ jsonlddb/extras/test_datapackage.py
@@ -3,7 +3,7 @@ import tempfile
 import shutil
 from datapackage import DataPackage
 from jsonlddb import JsonLDDatabase
-from jsonlddb.extras import examples, datapackage
+from jsonlddb.extras import examples, datapackage, generate
 
 try:
   from jsondiff import diff
@@ -14,6 +14,17 @@ except ImportError:
 
 def test_datapackage_example():
   db = JsonLDDatabase().insert(examples.familial_ownership)
+  tmpdir = tempfile.mkdtemp()
+  datapackage.to_datapackage(db, tmpdir)
+  pkg = DataPackage(os.path.join(tmpdir, 'datapackage.json'))
+  errors = pkg.errors
+  db_recover = datapackage.from_datapackage(tmpdir)
+  shutil.rmtree(tmpdir)
+  assert errors == [], errors
+  assert db.index.spo == db_recover.index.spo, diff(db.index.spo, db_recover.index.spo)
+
+def test_datapackage_random():
+  db = JsonLDDatabase().insert(list(generate.random_jsonld(100, 5, 2, 5, 2, 5)))
   tmpdir = tempfile.mkdtemp()
   datapackage.to_datapackage(db, tmpdir)
   pkg = DataPackage(os.path.join(tmpdir, 'datapackage.json'))
